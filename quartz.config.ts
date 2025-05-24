98c099120eb85e808b22401d220965f9fb000650
GizmoKowaski <duongphong270908c@gmail.com>
2025-05-24 17:24:07 +0700

    Quartz sync: May 24, 2025, 5:24 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -96,7 +96,6 @@ const config: QuartzConfig = {
       Plugin.Static(),
       Plugin.NotFoundPage(),
       // Comment out CustomOgImages to speed up build time
-      // Plugin.CustomOgImages(),
     ],
   },
 }
